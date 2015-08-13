1d317e447ec7fb7052655ccc3c73eab72d56fac4
YanLi <ly1992@qq.com>
2015-08-13 18:03:13 +0800

    new sui

--- sui/js/javascript.js
+++ sui/js/javascript.js
@@ -1,5 +1,17 @@
 $(function() {
-  $(".swiper-container").swiper();
+	//	启动swiper
+	$(".swiper-container").swiper();
+	//	icon高宽适应
+	$(".info-box.round .col-25").height($(".info-box.round .col-25").width() + 'px');
+	window.onresize = function() {
+			$(".info-box.round .col-25").height($(".info-box.round .col-25").width() + 'px');
+		}
+		//	汉堡菜单栏
+	$('.hamburger-icon,.panel-overlay').click(function() {
+		$('.hamburger-icon').toggleClass('active');
+	});
+//	$.alert('均为测试数据不要当真')
+$.open(function(){
+	alert();
 });
-
-
+});
